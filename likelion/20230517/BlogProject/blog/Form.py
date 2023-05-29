4e81bcb47c4c2abff3a11f8b9bf12b6598b2e060
chelmang <105014465+chelmang@users.noreply.github.com>
2023-05-30 01:55:50 +0900

    Delete likelion/20230517 directory

--- likelion/20230517/BlogProject/blog/Form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Post
-from django.contrib.auth.models import User
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ('title', 'excerpt', 'content', 'slug', 'published', 'status')
